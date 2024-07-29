a127363c0dc9efc9d9045602c84c04ce44bc34c6
JoelGJ <joelkirubakaran.gj@codentrix.com>
2024-07-29 19:52:31 +0530

    Navbar completed

---
+++ src/views/Index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Index.css';
+
+export default function Index() {
+    return (
+        <>
+            <header className='header'>
+                <div className='nav-container'>
+                    <ul className='nav-list'>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </header>
+        </>
+    );
+}
